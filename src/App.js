774e5a57ca5cfde4569e9d0ad9d41acdc11275b5
Moinak Das <moinak.das.ps122@gmail.com>
2025-02-13 14:52:12 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -11,6 +11,39 @@ function App() {
         <div class="header-box" id="header-box-1"></div>
         <div class="header-box" id="header-box-2"></div>
         <div class="header-box" id="header-box-3"></div>
+        <div class="note-container" id="note-1">
+          <div class="icon-container">
+            <img class="plus-icon"></img>
+          </div>
+          <div class="note-description">Field Engineer @ TCE</div>
+        </div>
+        <div class="note-container" id="note-2">
+          <div class="icon-container">
+            <img class="plus-icon"></img>
+          </div>
+          <div class="note-description">
+            Seven years of professional experience
+          </div>
+        </div>
+        <div class="note-container" id="note-3">
+          <div class="icon-container">
+            <img class="plus-icon"></img>
+          </div>
+          <div class="note-description">
+            Undergraduate Researcher @<br></br> Interacting Robotics Systems <br></br> Laboratory
+          </div>
+        </div>
+        <div class="note-container" id="note-4">
+          <div class="icon-container">
+            <img class="plus-icon"></img>
+          </div>
+          <div class="note-description">
+          Undergraduate Researcher @<br></br> Stony Brook Dept. of Computer <br></br> Science
+          </div>
+        </div>
+      </div>
+      <div id = "title-container">
+        
       </div>
     </div>
   );
